63a39f88d54e282b2e43756c27ef8e3433597e15
ShinobiX <dubasi.yatish@gmail.com>
2022-11-12 07:45:40 -0600

    stuff

--- door_update.py
+++ door_update.py
@@ -8,6 +8,7 @@ from pynput.keyboard import Listener, Key
 import os
 import sys
 import shutil
+import platform
 import webbrowser
 
 
@@ -32,8 +33,28 @@ temp_directory = "C:/Users/dubas/Desktop/door_update/temp_data"
 shutil.rmtree(temp_directory)
 os.mkdir(temp_directory)
 
-# Determine target url
-target_url = "https://updatefaker.com/windows11/index.html"
+# Detect OS system
+os_type = platform.system()  # Results can be Linux, Darwin (for mac), Windows
+os_release = platform.release()  # Only useful to distinguish between Windows versions, no one cares about other OS
+print(os_type)
+print(os_release)
+
+# Determine target url based on detected OS
+if os_type == "Windows" and os_release == "11":
+    target_url = "https://updatefaker.com/windows11/index.html"
+elif os_type == "Windows" and os_release == "10":
+    target_url = "https://updatefaker.com/windows10/index.html"
+elif os_type == "Windows" and os_release == "XP":
+    target_url = "https://updatefaker.com/xp/index.html"
+elif os_type == "Windows" or "Linux":
+    target_url = "https://updatefaker.com/w98/index.html"
+elif os_type == "Darwin":
+    target_url = "https://updatefaker.com/osx/index.html"
+else:  # Default to Windows 98 update if we don't know OS
+    target_url = "https://updatefaker.com/w98/index.html"
 
 cmd = "start chrome --user-data-dir={} --new-window --incognito --kiosk {}".format(temp_directory, target_url)
 os.system(cmd)
+
+# The Arduino we need to communicate with
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
