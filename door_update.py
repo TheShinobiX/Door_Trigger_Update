274143cdaf27b9fa56f2b6d7e0be604b65345315
ShinobiX <dubasi.yatish@gmail.com>
2022-11-12 10:21:21 -0600

    I did another thingy

--- door_update.py
+++ door_update.py
@@ -10,6 +10,7 @@ import sys
 import shutil
 import platform
 import webbrowser
+import random
 
 
 # -------------------------------
@@ -25,8 +26,18 @@ import webbrowser
 # -------------------------------
 # ------------ Start ------------
 # -------------------------------
-# Target URL when door is opened
-# https://updatefaker.com/windows11/index.html
+# Override device type with arguments
+# 11 = Windows 11
+# 10 = Windows 10
+# XP/xp = Windows XP
+# 98 = Windows 98
+# mac/Mac/MAC = mac
+# R/r = Random
+
+# Override flag to determine if we are overriding
+override_os = False
+if len(sys.argv) > 1:
+    override_os = True
 
 # temp data directory relative path
 temp_dir_rel = "temp_data"
@@ -41,8 +52,31 @@ os.mkdir(temp_directory)
 # Detect OS system
 os_type = platform.system()  # Results can be Linux, Darwin (for mac), Windows
 os_release = platform.release()  # Only useful to distinguish between Windows versions, no one cares about other OS
-print(os_type)
-print(os_release)
+
+# Set device types to desired device if override
+if override_os:
+    # If we are randomizing device type, or given arg is not present...
+    possible_args = ["11", "10", "XP", "xp", "98", "mac", "Mac", "MAC"]
+    if sys.argv[1] not in possible_args:
+        r_options = ["11", "10", "XP", "98", "mac"]
+        sys.argv[1] = random.sample(r_options, 1)[0]
+
+    # Now select device
+    if sys.argv[1] == "11":
+        os_type = "Windows"
+        os_release = "11"
+    elif sys.argv[1] == "10":
+        os_type = "Windows"
+        os_release = "10"
+    elif sys.argv[1] == "XP" or sys.argv[1] == "xp":
+        os_type = "Windows"
+        os_release = "XP"
+    elif sys.argv[1] == "98":
+        os_type = "Windows"
+        os_release = "98"
+    elif sys.argv[1] == "mac" or sys.argv[1] == "Mac" or sys.argv[1] == "MAC":
+        os_type = "Darwin"
+        os_release = "X"  # This is irrelevant for Mac
 
 # Determine target url based on detected OS
 if os_type == "Windows" and os_release == "11":
@@ -51,11 +85,9 @@ elif os_type == "Windows" and os_release == "10":
     target_url = "https://updatefaker.com/windows10/index.html"
 elif os_type == "Windows" and os_release == "XP":
     target_url = "https://updatefaker.com/xp/index.html"
-elif os_type == "Windows" or "Linux":
-    target_url = "https://updatefaker.com/w98/index.html"
 elif os_type == "Darwin":
     target_url = "https://updatefaker.com/osx/index.html"
-else:  # Default to Windows 98 update if we don't know OS
+else:  # Default to Windows 98 update if we don't know OS or if Linux
     target_url = "https://updatefaker.com/w98/index.html"
 
 cmd = "start chrome --user-data-dir={} --new-window --incognito --kiosk {}".format(temp_directory, target_url)
